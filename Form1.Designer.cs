74171f9701590a59f81612ed9fb95ea093e2c687
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-02-24 21:03:41 +0900

    First import

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace janmojannna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_remaining = new System.Windows.Forms.Label();
+            this.label_hint = new System.Windows.Forms.Label();
+            this.pictureBox_main = new System.Windows.Forms.PictureBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_turn = new System.Windows.Forms.Button();
+            this.label_accumulated = new System.Windows.Forms.Label();
+            this.button_get = new System.Windows.Forms.Button();
+            this.pictureBox_sub = new System.Windows.Forms.PictureBox();
+            this.timer_anime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sub)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_remaining
+            // 
+            this.label_remaining.AutoSize = true;
+            this.label_remaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_remaining.Location = new System.Drawing.Point(13, 13);
+            this.label_remaining.Name = "label_remaining";
+            this.label_remaining.Size = new System.Drawing.Size(136, 26);
+            this.label_remaining.TabIndex = 0;
+            this.label_remaining.Text = "のこり: 00 まい";
+            // 
+            // label_hint
+            // 
+            this.label_hint.AutoSize = true;
+            this.label_hint.Location = new System.Drawing.Point(357, 448);
+            this.label_hint.Name = "label_hint";
+            this.label_hint.Size = new System.Drawing.Size(111, 13);
+            this.label_hint.TabIndex = 1;
+            this.label_hint.Text = "※ スペースキーでめくる";
+            // 
+            // pictureBox_main
+            // 
+            this.pictureBox_main.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_main.Location = new System.Drawing.Point(32, 52);
+            this.pictureBox_main.Name = "pictureBox_main";
+            this.pictureBox_main.Size = new System.Drawing.Size(436, 348);
+            this.pictureBox_main.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_main.TabIndex = 2;
+            this.pictureBox_main.TabStop = false;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(391, 13);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(93, 26);
+            this.button_start.TabIndex = 3;
+            this.button_start.TabStop = false;
+            this.button_start.Text = "はじめから";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.Button_start_Click);
+            // 
+            // button_turn
+            // 
+            this.button_turn.Location = new System.Drawing.Point(240, 420);
+            this.button_turn.Name = "button_turn";
+            this.button_turn.Size = new System.Drawing.Size(111, 41);
+            this.button_turn.TabIndex = 4;
+            this.button_turn.Text = "めくる";
+            this.button_turn.UseVisualStyleBackColor = true;
+            this.button_turn.Click += new System.EventHandler(this.Button_turn_Click);
+            // 
+            // label_accumulated
+            // 
+            this.label_accumulated.AutoSize = true;
+            this.label_accumulated.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_accumulated.Location = new System.Drawing.Point(12, 420);
+            this.label_accumulated.Name = "label_accumulated";
+            this.label_accumulated.Size = new System.Drawing.Size(43, 13);
+            this.label_accumulated.TabIndex = 5;
+            this.label_accumulated.Text = "XX まい";
+            // 
+            // button_get
+            // 
+            this.button_get.Location = new System.Drawing.Point(62, 420);
+            this.button_get.Name = "button_get";
+            this.button_get.Size = new System.Drawing.Size(111, 41);
+            this.button_get.TabIndex = 6;
+            this.button_get.TabStop = false;
+            this.button_get.Text = "かくとく";
+            this.button_get.UseVisualStyleBackColor = true;
+            this.button_get.Click += new System.EventHandler(this.Button_get_Click);
+            // 
+            // pictureBox_sub
+            // 
+            this.pictureBox_sub.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_sub.Location = new System.Drawing.Point(32, 52);
+            this.pictureBox_sub.Name = "pictureBox_sub";
+            this.pictureBox_sub.Size = new System.Drawing.Size(436, 348);
+            this.pictureBox_sub.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_sub.TabIndex = 7;
+            this.pictureBox_sub.TabStop = false;
+            // 
+            // timer_anime
+            // 
+            this.timer_anime.Interval = 10;
+            this.timer_anime.Tick += new System.EventHandler(this.Timer_anime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 473);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_remaining);
+            this.Controls.Add(this.label_hint);
+            this.Controls.Add(this.button_turn);
+            this.Controls.Add(this.button_get);
+            this.Controls.Add(this.label_accumulated);
+            this.Controls.Add(this.pictureBox_sub);
+            this.Controls.Add(this.pictureBox_main);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ジャンモジャンナ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sub)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_remaining;
+        private System.Windows.Forms.Label label_hint;
+        private System.Windows.Forms.PictureBox pictureBox_main;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_turn;
+        private System.Windows.Forms.Label label_accumulated;
+        private System.Windows.Forms.Button button_get;
+        private System.Windows.Forms.PictureBox pictureBox_sub;
+        private System.Windows.Forms.Timer timer_anime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace janmojannna
+{
+    public partial class Form1 : Form
+    {
+        private int remaining_all;
+        private int type_all;
+        private int accumulated;
+        private int timer_cnt;
+        private int timer_cnt_max;
+
+        private const int speed         = 16;  // 10msec間にスライドするドット数
+        private const int each_max      =  5;  // 各画像の最大数
+        private const int deck_max      = 50;  // 使用する山札の割合(%)
+        private const string images_dir = @"Images\";
+
+        private Deck[] deck;
+
+        readonly Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MinimumSize = new Size(512, 512);
+            Set_position();
+
+            // 初期値の設定
+            remaining_all = 0;
+            type_all      = 0;
+            accumulated   = 0;
+            timer_cnt     = 0;
+
+            // 画像読み込み
+            Load_images();
+        }
+
+        private void Set_position()
+        {
+            int client_w = this.ClientSize.Width;
+            int client_h = this.ClientSize.Height;
+
+            button_start.Left       = client_w - button_start.Width - 16;
+            button_turn.Location    = new Point(client_w / 2, client_h - button_turn.Height - 16);
+            label_hint.Location     = new Point(button_turn.Left + button_turn.Width + 16, button_turn.Top + button_turn.Height - label_hint.Height);
+            pictureBox_main.Size    = new Size(client_w - 64, client_h - 128);
+            pictureBox_sub.Size     = new Size(client_w - 64, client_h - 128);
+            label_accumulated.Top   = button_turn.Top;
+            button_get.Top          = button_turn.Top;
+
+            timer_cnt_max = (pictureBox_sub.Width + 32) / speed;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Set_position();
+        }
+
+        private void Button_turn_Click(object sender, EventArgs e)
+        {
+            Change_image();
+        }
+
+        private void Load_images()
+        {
+            List<string> paths = new List<string>();
+            foreach(string exp in new string[] { "*.jpg", "*.png", "*.gif" })
+            {
+                paths.AddRange(Directory.GetFiles(images_dir, exp));
+            }
+
+            type_all = paths.Count;
+            remaining_all = type_all * each_max;
+
+            deck = new Deck[type_all];
+
+            for(int i=0; i<type_all; ++i)
+            {
+                deck[i] = new Deck(each_max, paths[i]);
+            }
+
+            Reduce_deck();
+
+            Set_labels();
+        }
+
+        private void Change_image()
+        {
+            int deck_idx;
+
+            --remaining_all;
+            if(remaining_all < 0)
+            {
+                // End
+                remaining_all = 0;
+                return;
+            }
+            else
+            {
+                ++accumulated;
+                Set_labels();
+
+                // Animation
+                if(timer_anime.Enabled)
+                {
+                    timer_cnt = 0;
+                    pictureBox_sub.Left = 32;
+                    pictureBox_sub.Image = pictureBox_main.Image;
+                }
+                else
+                {
+                    timer_anime.Enabled = true;
+                }
+            }
+
+            while (true)
+            {
+                deck_idx = r.Next(type_all);
+                if(deck[deck_idx].Remaining != 0)
+                {
+                    --deck[deck_idx].Remaining;
+                    break;
+                }
+            }
+
+            pictureBox_main.Image = deck[deck_idx].Image;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                Change_image();
+            }
+        }
+
+        private void Button_start_Click(object sender, EventArgs e)
+        {
+            button_turn.Focus();
+            accumulated = 0;
+            timer_cnt = 0;
+            remaining_all = type_all * each_max;
+            for(int i=0; i<type_all; ++i)
+            {
+                deck[i].Remaining = each_max;
+            }
+
+            Reduce_deck();
+
+            pictureBox_main.Image = null;
+            pictureBox_sub.Image = null;
+            Set_labels();
+        }
+
+        private void Set_labels()
+        {
+            label_remaining.Text = "のこり: " + remaining_all + " まい";
+            label_accumulated.Text = accumulated + " まい";
+        }
+
+        private void Button_get_Click(object sender, EventArgs e)
+        {
+            button_turn.Focus();
+            accumulated = 0;
+            Set_labels();
+        }
+
+        private void Timer_anime_Tick(object sender, EventArgs e)
+        {
+            ++timer_cnt;
+            if(timer_cnt < timer_cnt_max)
+            {
+                pictureBox_sub.Left += speed;
+            }
+            else
+            {
+                timer_cnt = 0;
+                pictureBox_sub.Left = 32;
+                pictureBox_sub.Image = pictureBox_main.Image;
+
+                timer_anime.Enabled = false;
+            }
+        }
+
+        private void Reduce_deck()
+        {
+            int deck_idx;
+            for(int _i=remaining_all * deck_max / 100, _j=remaining_all; _i < _j; ++_i)
+            {
+                --remaining_all;
+                while (true)
+                {
+                    deck_idx = r.Next(type_all);
+                    if (deck[deck_idx].Remaining != 0)
+                    {
+                        --deck[deck_idx].Remaining;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    internal class Deck
+    {
+        private int remaining;
+        private Bitmap image;
+
+        public Deck(int remaining, string path)
+        {
+            image = new Bitmap(path);
+            this.remaining = remaining;
+        }
+
+        public int Remaining
+        {
+            set { this.remaining = value; }
+            get { return this.remaining; }
+        }
+
+        public Bitmap Image
+        {
+            get { return this.image; }
+        }
+    }
+
+}
